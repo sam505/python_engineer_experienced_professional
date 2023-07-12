5e38759164c68023b12e8b291e01143b64f8c854
sam505 <mainasamuel505@gmail.com>
2023-07-12 20:36:19 +0300

    Update boolean values

--- main_test.py
+++ main_test.py
@@ -9,7 +9,7 @@ def test_one_read_json():
       "appName": "ABCDEFG",
       "eventType": "ABCDEFGHIJ",
       "subEventType": "ABCDEFGHIJKLMNOPQRSTU",
-      "sensitive": true
+      "sensitive": True
     },
     "message": {
       "battle": {
@@ -50,7 +50,7 @@ def test_one_read_json():
               "countryCode": "ABCDEFGH",
               "orientation": "ABCDEFGHIJKLMNOPQRSTUVWXY"
             },
-            "creator": false,
+            "creator": False,
             "ranking": 498,
             "numberOfTrades": 862,
             "performance": "ABCDEFGHIJKLMNOPQRSTUVW"
@@ -79,75 +79,7 @@ def test_one_read_json():
 
 def test_two_read_json():
     filepath = "data/data_not_exist.json"
-
-    actual_data = {
-    "attributes": {
-      "appName": "ABCDEFG",
-      "eventType": "ABCDEFGHIJ",
-      "subEventType": "ABCDEFGHIJKLMNOPQRSTU",
-      "sensitive": true
-    },
-    "message": {
-      "battle": {
-        "id": "ABCDEFGHIJKLMNOPQR",
-        "name": "ABCDEFGHIJKLMNOPQRSTUVWX",
-        "orientation": "ABCDEFGHIJKLMNO",
-        "settings": {
-          "minParticipants": 942,
-          "maxParticipants": 641,
-          "battleType": "ABCDEFGHIJKLMN",
-          "wagerType": "ABCDEFGHIJKLMNOPQRSTUVW",
-          "countdown": 69,
-          "duration": 200,
-          "archetype": {
-            "name": "ABCDEFGHIJKLMNOPQRS",
-            "iconId": "ABCDEFGHIJKLMNOPQRST"
-          }
-        },
-        "status": "ABCDEFGHIJKL",
-        "creationTime": 240,
-        "startTime": 626,
-        "endTime": 353,
-        "creator": {
-          "id": "ABCDEFGHIJKLMNOPQRSTUVWXYZA",
-          "nickname": "ABCDEFGHIJKLMNOPQ",
-          "title": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-          "accountType": "ABCDE",
-          "countryCode": "ABCD",
-          "orientation": "ABCDEFGHIJKLMNO"
-        },
-        "participants": [
-          {
-            "user": {
-              "id": "ABCDEFGHIJKLMN",
-              "nickname": "ABCDEFGHIJKLMN",
-              "title": "ABCDEFGHIJK",
-              "accountType": "ABCDEFGHIJKLMNOPQRSTUVWX",
-              "countryCode": "ABCDEFGH",
-              "orientation": "ABCDEFGHIJKLMNOPQRSTUVWXY"
-            },
-            "creator": false,
-            "ranking": 498,
-            "numberOfTrades": 862,
-            "performance": "ABCDEFGHIJKLMNOPQRSTUVW"
-          }
-        ]
-      },
-      "joiner": {
-        "id": "ABCDEFGHIJKLMNOPQRSTUVWXYZAB",
-        "nickname": "ABCDEFGHIJKLMNO",
-        "title": "ABCDEFGHIJKLMNOPQRSTUVWXYZABC",
-        "accountType": "ABCDEFGHIJKLMNOPQRS",
-        "countryCode": "ABCDEFGHIJKLMNOPQR",
-        "orientation": "ABCDEFGHIJKLMNOPQR"
-      },
-      "participantIds": [
-        "ABCDEFGHIJKLMNOPQRST",
-        "ABCDEFGHIJKLMNOPQRSTUVWXY"
-      ]
-    }
-  }
     
     read_data = read_json(filepath)
 
-    assert actual_data == read_data
+    assert read_data is None
