81958b676ad538d4fb5fa198278179339904ecf9
sam505 <mainasamuel505@gmail.com>
2023-07-13 03:07:16 +0300

    Add generate JSON schema solution

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ import os
 import json
 import logging
 
-
+# add logger to help with debugging
 logging.basicConfig(filename="newfile.log",
-                    format='%(asctime)s %(message)s',
+                    format='%(asctime)s - %(levelname)s - %(message)s',
                     filemode='w')
 logger = logging.getLogger()
  
 logger.setLevel(logging.DEBUG)
+
+# Data types to be used when generating schema
+TYPES = {
+        type(1): 'integer',
+        type(1.2): 'float',
+        type("abc"): 'string',
+        type(True): 'boolean',
+        type([]): 'array',
+        type({}): 'object',
+        }
 
 
 def read_json(filepath:str) -> dict:
@@ -23,10 +33,10 @@ def read_json(filepath:str) -> dict:
     try:
         with open(filepath, "r") as file:
             obj = json.load(file)
-            logger.debug("JSON file exists...")
+            logger.debug(f"JSON file {filepath} exists...")
             return obj
     except FileNotFoundError:
-        logger.debug("JSON file not found")
+        logger.error("JSON file not found")
 
 
 def generate_schema(obj: dict) -> dict:
@@ -38,39 +48,96 @@ def generate_schema(obj: dict) -> dict:
     Returns:
         dict: JSON object representing the schema of the JSON object passed to the function
     """
-    TYPES = {
-        type(1): 'integer',
-        type(1.2): 'float',
-        type("abc"): 'string',
-        type(True): 'boolean',
-        type([]): 'enum',
-        type({}): 'array',
-        }
-
-    logger.debug("Generating schema...")
+    
+    logger.debug(f"Generating schema for {obj}...")
     schema = {}
+    try:
+        for obj_key in obj.keys():
+            # initialize a skeleton with the required keys and required key set to false
+            schema_format = {
+            "type": "",
+            "tag": "",
+            "description": "",
+            "required": False
+            }
+            new_obj = obj[obj_key]
+            logger.debug(f"Generating for key: {obj_key}")
 
-    schema_format = {
-        "type": "",
-        "tag": "",
-        "description": "",
-        "required": False
-    }
+            attribute_type = TYPES[type(new_obj)]
 
-    for obj_key in obj:
-        schema_format["type"] = TYPES[type(obj[obj_key])]
-        schema[obj_key] = schema_format
+            # only captute attributes ONLY in message key
+            if attribute_type in ["array", "object"] and obj_key != "attributes":
+                logger.info(f"Getting schema through loop for {new_obj}...", )
+                if attribute_type == "object":
+                    schema_format["properties"] = generate_schema(new_obj)
+                else:
+                    schema_format["items"] = generate_schema(new_obj)
+            
+            schema_format["type"] = attribute_type
+            schema[obj_key] = schema_format
 
+    except AttributeError:
+        schema = []
+        for new_obj in obj:
+            # initialize a skeleton with the required keys and required key set to false
+            schema_format = {
+            "type": "enum",
+            "tag": "",
+            "description": "",
+            "required": False
+            }
+
+            attribute_type = TYPES[type(new_obj)]
+
+            # only captute attributes ONLY in message key
+            if attribute_type in ["array", "object"]:
+                logger.info(f"Getting schema through loop for {new_obj}...", )
+                schema_format["properties"] = generate_schema(new_obj)
+                schema_format["type"] = "array"
+            
+            schema.append(schema_format)
+        
+    
     return schema
 
 
+def save_schema(obj: dict, filename: str) -> str:
+    """Gets the generated schema object and the filename of the JSON file and then dumps the JSON object in the schema folder
+
+    Args:
+        obj (dict): generated schema dictionary object
+        filename (str): filename of the JSON file
+
+    Returns:
+        str: path of the saved JSON schema file 
+    """
+    schema_dir = "schema"
+
+    # check if schema dir exists and create if it does not exist
+    if not os.path.isdir(schema_dir):
+        os.mkdir(schema_dir)
+
+    new_filename = filename.replace("data", "schema")
+    filepath = os.path.join(schema_dir, new_filename)
+    with open(filepath, "w") as file:
+            json.dump(
+                {
+                    "$schema": "http://json-schema.org/draft-04/schema#",
+                    "type": "object",
+                    "properties": obj
+                }
+                , file, indent=4)
+    logger.debug(obj)
+
+    return None
+
 
 def main():
-    folder = "data"
-    for filename in os.listdir(folder):
-        read_json(os.path.join(folder, filename))
-
-    pass
+    data_folder = "data"
+    for filename in os.listdir(data_folder):
+        json_obj = read_json(os.path.join(data_folder, filename))
+        schema = generate_schema(json_obj)
+        save_schema(schema, filename)
 
 
 
